288f5ac3242574beed49d25e93bcfc86ec29056d
Brian Sitati <briansit93@gmail.com>
2025-01-03 17:00:47 +0300

    added style to navbar when scrolled

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 
-//   anime({
-//     targets:  '#right-hero-image',
-//     translateX: ['0%', '5%'], 
-//     duration: 5000, 
-//     easing: 'linear', 
-//     loop: true,
-//     direction: 'alternate', 
-//   });
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    
+    if (window.scrollY > 50) {
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
+
+
+
+
+  anime({
+    targets:  '#right-hero-image',
+    translateX: ['0%', '5%'], 
+    duration: 5000, 
+    easing: 'linear', 
+   loop: true,
+    direction: 'alternate', 
+  });
